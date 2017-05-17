a4a0fffdf2ce4c4b104ea3d450ee127d1835ac49
Matheus J. A. Theodoro <matheusjat@gmail.com>
2017-05-17 10:21:29 -0300

    Primeiro Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public int speed = 1;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = 0f;
+        float moveVertical = 0f;
+
+        if ((Input.GetAxis("Horizontal") != 0) || (Input.GetAxis("Vertical") != 0))
+        {
+            moveHorizontal = Input.GetAxis("Horizontal");
+            moveVertical = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            moveHorizontal = Input.acceleration.x;
+            moveVertical = Input.acceleration.y;
+        }
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+
+    }
+}
+
+
---
+++ Assets/Scripts/Tiro.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Tiro : MonoBehaviour {
+
+    private AudioSource somDisparo;
+    private bool disparo;
+    private WaitForSeconds duracaoDisparo = new WaitForSeconds(0.07f);
+
+    // Use this for initialization
+    void Start () {
+        somDisparo = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            disparo = false;
+            StartCoroutine(EfeitoDisparo());
+
+        }
+		
+	}
+
+    public void Disparo()
+    {
+        disparo = true;
+        print("Teste");
+    }
+
+    private IEnumerator EfeitoDisparo()
+    {
+        somDisparo.Play();
+        yield return duracaoDisparo;
+    }
+}
