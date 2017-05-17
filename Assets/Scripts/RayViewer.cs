dd3b88eab39c2273dc9dd155b89fe1b87d19d92a
Matheus J. A. Theodoro <matheusjat@gmail.com>
2017-05-17 20:30:02 -0300

    Segundo Commit

---
+++ Assets/Scripts/RayViewer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayViewer : MonoBehaviour {
+
+    private Camera fpsCam;
+
+    public float distanciaDisparo = 50f;
+
+    // Use this for initialization
+    void Start () {
+
+        fpsCam = GetComponentInParent<Camera>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 origemLinha = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+
+        Debug.DrawRay(origemLinha, fpsCam.transform.forward * distanciaDisparo);
+
+	}
+}
--- Assets/Scripts/Tiro.cs
+++ Assets/Scripts/Tiro.cs
@@ -8,18 +8,46 @@ public class Tiro : MonoBehaviour {
     private AudioSource somDisparo;
     private bool disparo;
     private WaitForSeconds duracaoDisparo = new WaitForSeconds(0.07f);
+    private Camera fpsCam;
+    public float distanciaDisparo = 5000f;
+    public float forcaDisparo = 1000f;
+    public GameObject canoArma;
+    private LineRenderer laser;
 
     // Use this for initialization
     void Start () {
         somDisparo = GetComponent<AudioSource>();
-	}
+        fpsCam = GetComponent<Camera>();
+        laser = GetComponent<LineRenderer>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
         if (Input.GetButtonDown("Fire1"))
         {
             disparo = false;
             StartCoroutine(EfeitoDisparo());
+            
+            Vector3 origemLinha = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+
+            laser.SetPosition(0, canoArma.transform.position);
+
+            RaycastHit pontoColisao;
+
+            if (Physics.Raycast(origemLinha, fpsCam.transform.forward,
+                out pontoColisao, distanciaDisparo))
+            {
+                laser.SetPosition(1, pontoColisao.point);
+                if (pontoColisao.rigidbody != null)
+                {
+                    pontoColisao.rigidbody.AddForce(-pontoColisao.normal * forcaDisparo);
+                }
+            }else
+            {
+                laser.SetPosition(1, canoArma.transform.position + fpsCam.transform.forward * distanciaDisparo);
+            }
+
 
         }
 		
@@ -34,6 +62,8 @@ public class Tiro : MonoBehaviour {
     private IEnumerator EfeitoDisparo()
     {
         somDisparo.Play();
+        laser.enabled = enabled;
         yield return duracaoDisparo;
+        laser.enabled = false;
     }
 }
